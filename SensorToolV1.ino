d5a3527627a1d5332463dc4cc398da8df07c1298
TsuyoshiPerogi <86621780+TsuyoshiPerogi@users.noreply.github.com>
2022-01-03 20:14:34 -0500

    Update SensorToolV1.ino

--- SensorToolV1.ino
+++ SensorToolV1.ino
@@ -57,18 +57,18 @@ float X10;
 float FmultiMap(float val, float * _in, float * _out, uint8_t size)
 {
   
-  // val = constrain(val, _in[0], _in[size-1]);
+  
   if (val <= _in[0]) return _out[0];
   if (val >= _in[size-1]) return _out[size-1];
 
-  // search right interval
-  uint8_t pos = 1;  // _in[0] allready tested
+ 
+  uint8_t pos = 1;  
   while(val > _in[pos]) pos++;
 
-  // this will handle all exact "points" in the _in array
+  
   if (val == _in[pos]) return _out[pos];
 
-  // interpolate in the right segment for the rest
+  
   return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
 }
   
